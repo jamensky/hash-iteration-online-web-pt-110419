153e9c5729460244d0a9dfd1b8b2fab96c13b19a
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-20 19:26:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,6 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     puts "Happy Birthday #{name}! You are now #{age} years old!"
-  end
 end
 
 def age_appropriate_birthday()
